2dae3f1495d830f3c2153a69021a9da71aaa209d
teesha17 <teeshu.2179@gmail.com>
2023-12-19 11:48:53 +0530

    added playlist

--- src/screens/library/library.js
+++ src/screens/library/library.js
@@ -1,7 +1,28 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import APIKit from "../../spotify"
 export default function  Library() {
+
+  const[playlists,setPlaylists]=useState(null);
+
+  useEffect(() => {
+    console.log("Before API request");
+    APIKit.get("me/playlists")
+      .then((response) => {
+        setPlaylists(response.data.items);
+        console.log("API Response:", response.data);
+      })
+      .catch((error) => {
+        console.error("API Error:", error);
+      })
+      .finally(() => {
+        console.log("After API request");
+      });
+  }, []);
   return (
-    <div className='screen-container'></div>
+    <div className='screen-container'>
+      {playlists?.map(playlists=>
+        <div>{playlists.name}</div>
+      )}
+    </div>
   )
 }
